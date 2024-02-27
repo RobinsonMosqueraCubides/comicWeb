669263bc715c937d928178ad09136babf5ed19e8
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-02-26 20:41:51 -0500

    feat: :sparkles: js section DC comics
    Se agrega el js de la parte de dc Comics, tambien se agrega las funciones del evento clic y se completa la informacion del json de Marvel

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const marvelSuperheroes = document.querySelector('.marvelSuperheroes .cards');
+const dcSuperheroes = document.querySelector('.dcSuperheroes .cardsDc');
 async function getData() {
     const response = await fetch('storage/data.json')
     return response.json()
 }
+const dataJson = await getData();
 try {
-    const dataJson = await getData();
     for (const key in dataJson) {
         if (key === "marvelSuperheroes") {
             for (const heroe in dataJson[key]) {
@@ -19,10 +20,56 @@ try {
             </figure>`
             let tdiv = document.createElement('div');
             tdiv.innerHTML = baseCard;
-            marvelSuperheroes.appendChild(tdiv.firstChild);
+            marvelSuperheroes.appendChild(tdiv);
+            }
+        }
+        if (key === "dcSuperheroes") {
+            for (const heroe in dataJson[key]) {
+                let info = dataJson[key][heroe];
+                let baseCard = `<figure class="cartaPersonajeDc">
+                <section class="imagenDc">
+                    <img src="${info.imagen}" alt="noCarga">
+                </section>
+                <section class="nombreDc">
+                    <p>${info.nombre}</p>
+                </section>
+            </figure>`
+            let tdiv = document.createElement('div');
+            tdiv.innerHTML = baseCard;
+            dcSuperheroes.appendChild(tdiv);
             }
         }
     }
 } catch (error) {
     
+}
+
+
+    var cardsElement = document.querySelector('.cards');
+    cardsElement.addEventListener('click', function (event) {
+        if (event.target.closest('.cartaPersonaje')) {
+            var nombreElement = event.target.closest('.cartaPersonaje');
+            var contenidoNombre = nombreElement.querySelector('p').textContent;
+            selector(contenidoNombre);
+        }
+    });
+    var cardsElement = document.querySelector('.cardsDc');
+    cardsElement.addEventListener('click', function (event) {
+        if (event.target.closest('.cartaPersonajeDc')) {
+            var nombreElement = event.target.closest('.cartaPersonajeDc');
+            var contenidoNombre = nombreElement.querySelector('p').textContent;
+            selector(contenidoNombre);
+        }
+    });
+function selector(nombreSuper){
+    for (let i = 0; i < dataJson.marvelSuperheroes.length; i++){
+        if(dataJson.marvelSuperheroes[i].nombre === nombreSuper){
+            console.log(dataJson.marvelSuperheroes[i].descripcion);
+        }
+    }
+    for (let i = 0; i < dataJson.dcSuperheroes.length; i++){
+        if(dataJson.dcSuperheroes[i].nombre === nombreSuper){
+            console.log(dataJson.dcSuperheroes[i].descripcion);
+        }
+    }
 }
